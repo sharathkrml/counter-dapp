38ec01523b6936d50d3e9f2bba925e4ddc70a0ae
sharath V <sharathkrml@gmail.com>
2022-02-17 22:46:54 +0530

    getCount completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import { useState, useEffect } from "react";
+import { ABI, ADDRESS } from "./const";
+import { ethers } from "ethers";
 function App() {
   const [count, setCount] = useState(0);
 
@@ -8,12 +10,24 @@ function App() {
       alert("You dont have a wallet");
     } else {
       requestAccount();
+      getCount();
     }
   }, []);
   const requestAccount = async () => {
     await window.ethereum.request({
       method: "eth_requestAccounts",
     });
+  };
+
+  const getCount = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const CounterContract = new ethers.Contract(ADDRESS, ABI, provider);
+    try {
+      const data = await CounterContract.count();
+      setCount(data.toString());
+    } catch (err) {
+      console.log("Error:", err);
+    }
   };
 
   const increment = () => {
---
+++ src/const.js
@@ -0,0 +1,30 @@
+export const ABI = [
+  {
+    inputs: [],
+    name: "count",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "decrement",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "increment",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+export const ADDRESS = "0xaB684aAAcbaD42D7a9B915C077f192d82d4cdd91";
