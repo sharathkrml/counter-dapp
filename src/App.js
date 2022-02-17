a29ecae10877f9d288ec09c3b7304ad582c17a9e
sharath V <sharathkrml@gmail.com>
2022-02-17 22:59:26 +0530

    increment and decrement finished

--- src/App.js
+++ src/App.js
@@ -30,18 +30,29 @@ function App() {
     }
   };
 
-  const increment = () => {
-    setCount((prevCount) => prevCount + 1);
+  const increment = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const CounterContract = new ethers.Contract(ADDRESS, ABI, signer);
+    const txn = await CounterContract.increment();
+    setCount("👆👆👆");
+    await txn.wait();
+    getCount();
   };
-  const decrement = () => {
-    setCount((prevCount) => prevCount - 1);
+  const decrement = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const CounterContract = new ethers.Contract(ADDRESS, ABI, signer);
+    const txn = await CounterContract.decrement();
+    setCount("👇👇👇");
+    await txn.wait();
+    getCount();
   };
   return (
     <div className="App">
       <button onClick={increment}>Increment</button>
       <div>{count}</div>
       <button onClick={decrement}>Decrement</button>
-      <button onClick={() => requestAccount()}>Connect Wallet</button>
     </div>
   );
 }
